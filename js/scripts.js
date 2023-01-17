dc40a7b12c1f5d1e01bbcff1066e18d826aed2e7
Spectrum5 <toskusergio@gmail.com>
2023-01-17 21:29:22 +0100

    Aggiunta lista e immagini

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+let team = [
+    {
+        name: `Wayne Barnett`,
+        job: `Founder & CEO`,
+        image: `img/wayne-barnett-founder-ceo.jpg`
+    },
+    {
+        name: `Angela Caroll`,
+        job: `Chief Editor`,
+        image: `img/angela-caroll-chief-editor.jpg`
+    },
+    {
+        name: `Walter Gordon`,
+        job: `Office Manager`,
+        image: `img/walter-gordon-office-manager.jpg`
+    },
+    {
+        name: `Angela Lopez`,
+        job: `Social Media Manager`,
+        image: `img/angela-lopez-social-media-manager.jpg`
+    },
+    {
+        name: `Scott Estrada`,
+        job: `Developer`,
+        image: `img/scott-estrada-developer.jpg`
+    },
+    {
+        name: `Barbara Ramos`,
+        job: `Graphic Designer`,
+        image: `img/barbara-ramos-graphic-designer.jpg`
+    }
+]
